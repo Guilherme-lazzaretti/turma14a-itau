2457d9c7c6864c178383dd2ccaf0dcf58bfa1e77
Guilherme-lazzaretti <guilherme.s.lazzaretti@gmail.com>
2021-02-04 15:54:02 -0300

    test5

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
 <h3> Teste Guilherme :D <h4>
 
 <div style="display:flex">
+  <div style="width:200px;height:400px;background:yellow;padding:50px;margin:10px">
+    Um teste em um quadrado
+    <br>
+    <img src="bike1.jfif" style="width: 200px">
+    <br><br>
+    R$ 550,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+  </div>
 
-<div style="width:200px;height:200px;background:yellow;color:green;padding:100px;margin:20px">
-  um teste em quadrado 
-  <br>
-  <ing src="bike1.jfif" style="width:50px">
+  <div style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
+    Um teste em um quadrado
+    <br>
+    <img src="bike2.jfif" style="width: 200px">
+   <br><br>
+    R$ 850,00
+    style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10p<br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" x">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
+  </div>
 </div>
-
-<div style="width:100px;height:400px;background:blue;color:white;padding:50px;margin:10px">
-  um teste em quadrado 
-  <br>
-  <ing src="bike2.jfif" style="width:100px">
-</div>
-  
