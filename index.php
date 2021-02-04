cecd265a430a3ae3e780a15519cbce6c0ca29ece
Guilherme-lazzaretti <guilherme.s.lazzaretti@gmail.com>
2021-02-04 14:58:20 -0300

    Teste3

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<h1>RODAR LISO </h1>
+<h1> ----------- RODAR LISO----------- </h1>
 
 <h3> Teste Guilherme :D <h4>
 
-  <div style="width:200px;height:200px;background:yellow">
-  um teste em quadrado 
-  </div>
-gi
+<div style="display:flex">
 
+<div style="width:200px;height:200px;background:yellow;color:green;padding:100px;margin:20px">
+  um teste em quadrado 
+</div>
+
+<div style="width:100px;height:400px;background:blue;color:white;padding:50px;margin:10px">
+  um teste em quadrado 
+</div>
