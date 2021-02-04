de958bd386a55eff9c71c48e3cb0f57e350650ac
Guilherme-lazzaretti <guilherme.s.lazzaretti@gmail.com>
2021-02-04 15:14:42 -0300

    teste4

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 
 <div style="width:200px;height:200px;background:yellow;color:green;padding:100px;margin:20px">
   um teste em quadrado 
+  <br>
+  <ing src="bike1.jfif" style="width:50px">
 </div>
 
 <div style="width:100px;height:400px;background:blue;color:white;padding:50px;margin:10px">
   um teste em quadrado 
+  <br>
+  <ing src="bike2.jfif" style="width:100px">
 </div>
+  
