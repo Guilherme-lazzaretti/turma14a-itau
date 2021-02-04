0bc72f997e7b3df27c0e7321af57ddee29be16b3
Guilherme-lazzaretti <guilherme.s.lazzaretti@gmail.com>
2021-02-04 14:16:21 -0300

    testedevops

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
+<h1>RODAR LISO </h1>
 
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
+<h3> Teste Guilherme :D <h4>
 
-<h5>Tentar sempre, desistir jamais!!!<h5>
-  
-  <h4> Teste Guilherme :D <h4>
+  <div style="width:200px:height:200px:background:yellow">
+  um teste em quadrado 
+  </div>
+
+
